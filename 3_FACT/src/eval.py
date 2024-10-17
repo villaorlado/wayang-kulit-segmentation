946cb0d9471c2a35d57eab3332388780b8eb9d6f
wootoonooyoo <sphericalpenguins@gmail.com>
2024-10-18 00:08:06 +0800

    allowed cli calling of eval

--- 3_FACT/src/eval.py
+++ 3_FACT/src/eval.py
@@ -45,80 +45,94 @@ def read_args_json_file(args_json_file):
     return cfg
 
 
-# Parameters
-json_file_path = "args.json"
-epoch = 780
-wandb_run_nb = 5
+def get_preds(args_json_filepath:str):
+    """
+    [Arguments]
+        args_json_filepath: path to the json file containing the arguments (in log folder)
+        epoch: epoch number to load the model from
+        output_json_filepath: path to the json file where the predictions will be saved
 
-# Read config file
-cfg = read_args_json_file(json_file_path)
+    [Returns]
+        nothing. Saves the predictions to the {output_json_filepath}
 
-# Get dataset
-dataset, test_dataset = create_dataset(cfg)
-dataset_name = cfg.dataset
-logdir = cfg.logdir
+    [Usage]
+        python3 -m src.eval --args_json_filepath "args.json" --epoch 40 --output_json_filepath "preds.json"
+    """
 
-# Load Model
-if dataset_name == 'epic-kitchens':
-    from .models.blocks_SepVerbNoun import FACT
-    model = FACT(cfg, dataset.input_dimension)
-else:
-    from .models.blocks import FACT 
-    model = FACT(cfg, dataset.input_dimension, dataset.nclasses)
+    # Read config file
+    cfg = read_args_json_file(args_json_filepath)
 
-# Read Model weights
-weights = f'{logdir}/ckpts/network.iter-{epoch}.net'
-weights = torch.load(weights, map_location='cpu')
-if 'frame_pe.pe' in weights:
-    del weights['frame_pe.pe']
-model.load_state_dict(weights, strict=False)
-model.eval().cuda()
+    # Get dataset
+    dataset, test_dataset = create_dataset(cfg)
+    dataset_name = cfg.dataset
+    logdir = cfg.logdir
 
-# Dataloaders
-train_loader  = DataLoader(dataset, 1, shuffle=False)
-test_loader  = DataLoader(test_dataset, 1, shuffle=False)
-
-for loader_nb, loader in enumerate([train_loader, test_loader]):
-
-    # Metrics
-    ckpts = []
-    ckpt = Checkpoint(-1, bg_class=([] if cfg.eval_bg else dataset.bg_class))
-
-    # Storage variables
-    all_results = {}
-
-    # Generate predictions
-    for vname, batch_seq, train_label_list, eval_label in tqdm(loader):
-        seq_list = [ s.cuda() for s in batch_seq ]
-        train_label_list = [ s.cuda() for s in train_label_list ]
-        video_saves = model(seq_list, train_label_list)
-        save_results(ckpt, vname, eval_label, video_saves)
-        
-        local_results = {}
-        label_array = []
-        pred_array = []
-        
-        for label, pred in zip(video_saves[0]['pred'], eval_label[0]):
-            label_array.append(int(label))
-            pred_array.append(int(pred))
-            
-        local_results["label"] = label_array
-        local_results["pred"] = pred_array
-        all_results[vname[0]] = local_results
-    
-    # Writing to sample.json
-    json_object = json.dumps(all_results, indent=4)
-    if loader_nb == 0:
-        loader_name = 'train'
+    # Load Model
+    if dataset_name == 'epic-kitchens':
+        from .models.blocks_SepVerbNoun import FACT
+        model = FACT(cfg, dataset.input_dimension)
     else:
-        loader_name = 'test'
-    with open(f"run{wandb_run_nb}_iter{iter}_{loader_name}.json", "w") as outfile:
-        outfile.write(json_object)
-    print(f"saved run{wandb_run_nb}_iter{iter}_{loader_name}.json")
-    
-    # Metrics
-    ckpt.compute_metrics()
-    ckpts.append(ckpt)
-    print(utils.easy_reduce([c.metrics for c in ckpts]))
+        from .models.blocks import FACT 
+        model = FACT(cfg, dataset.input_dimension, dataset.nclasses)
 
+    # Read Model weights
+    weights = f'{logdir}/ckpts/network.iter-{epoch}.net'
+    weights = torch.load(weights, map_location='cpu')
+    if 'frame_pe.pe' in weights:
+        del weights['frame_pe.pe']
+    model.load_state_dict(weights, strict=False)
+    model.eval().cuda()
+
+    # Dataloaders
+    train_loader = DataLoader(dataset, 1, shuffle=False)
+    test_loader  = DataLoader(test_dataset, 1, shuffle=False)
+
+    for loader in [train_loader, test_loader]:
+
+        # Metrics
+        ckpts = []
+        ckpt = Checkpoint(-1, bg_class=([] if cfg.eval_bg else dataset.bg_class))
+
+        # Storage variables
+        all_results = {}
+
+        # Generate predictions
+        for vname, batch_seq, train_label_list, eval_label in tqdm(loader):
+            seq_list = [ s.cuda() for s in batch_seq ]
+            train_label_list = [ s.cuda() for s in train_label_list ]
+            video_saves = model(seq_list, train_label_list)
+            save_results(ckpt, vname, eval_label, video_saves)
+            
+            local_results = {}
+            label_array = []
+            pred_array = []
+            
+            for label, pred in zip(video_saves[0]['pred'], eval_label[0]):
+                label_array.append(int(label))
+                pred_array.append(int(pred))
+                
+            local_results["label"] = label_array
+            local_results["pred"] = pred_array
+            all_results[vname[0]] = local_results
         
+        # Writing to sample.json
+        json_object = json.dumps(all_results, indent=4)
+        with open(f"{output_json_filepath}", "w") as outfile:
+            outfile.write(json_object)
+        print(f"Saved predictions of epoch={epoch} to {output_json_filepath}")
+        
+        # Metrics
+        ckpt.compute_metrics()
+        ckpts.append(ckpt)
+        print(utils.easy_reduce([c.metrics for c in ckpts]))
+
+            
+if __name__ == "__main__":
+    
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--args_json_filepath", type=str, help="path to the json file containing the arguments (in log folder)")
+    parser.add_argument("--epoch", type=int, help="epoch number to load the model from")
+    parser.add_argument("--output_json_filepath", type=str, help="path to the json file where the predictions will be saved")
+    args = parser.parse_args()
+    
+    get_preds(args.args_json_filepath, args.epoch, args.output_json_filepath)
